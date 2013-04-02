b9e0a592e4c0ff148d7233327b738b5a594c478c
Geoff MacDonald <geoffmacd@Geoffs-MacBook-Air.local>
2013-04-01 23:49:53 -0400

    added badge to app icon

--- RunnersCompass/AppDelegate.m
+++ RunnersCompass/AppDelegate.m
@@ -283,6 +283,8 @@
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     
+    //set badge to nothing
+    [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
     
     return YES;//to indicate success
 }
@@ -291,6 +293,8 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -298,6 +302,15 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
+    
+    if(self.backVC.run.live)
+    {
+        //set badge to 1
+        [UIApplication sharedApplication].applicationIconBadgeNumber = 1;
+    }
+    else{
+        [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+    }
     
     //alert logger view
     [self.backVC setInBackground:true];
