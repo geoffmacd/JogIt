c230685e1ac0fd8fa8c606adc9d43999a8fa1aa0
geoffmacd <geoffmacd@gmail.com>
2013-04-29 19:04:59 -0400

    increased requirement to enable auto pause

--- RunnersCompass/Logger.h
+++ RunnersCompass/Logger.h
@@ -48,8 +48,8 @@
 #define autoZoomPeriod 6 //seconds before auto zoom
 #define userDelaysAutoZoom 15 //second delays before autozoom
 #define reloadMapIconPeriod 6 // second reload map icon period
-#define autoPauseDelay 10 // seconds before app pauses
-#define autoPauseSpeed 0.5 //m/s speed app pauses at 
+#define autoPauseDelay 15 // seconds before app pauses
+#define autoPauseSpeed 0.25 //m/s speed app pauses at 
 #define minSpeedUnpause 1 //m/s
 #define paceChartMaxYMin 1//m/s
 #define paceChartCutoffPercent 0.05//%
